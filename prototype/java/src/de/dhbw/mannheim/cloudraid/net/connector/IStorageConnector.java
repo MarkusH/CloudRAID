12cfc11e92142b07bea543b971517806b04a2f85
Florian Bausch <studium@fbausch.de>
2011-11-25 17:15:33 +0100

    updated IStorageConnector

--- prototype/java/src/de/dhbw/mannheim/cloudraid/net/connector/IStorageConnector.java
+++ prototype/java/src/de/dhbw/mannheim/cloudraid/net/connector/IStorageConnector.java
@@ -16,20 +16,48 @@ public interface IStorageConnector {
 	 * 
 	 * @return true, if the connection could be established; false, if not.
 	 */
-	public boolean connect();
+	public boolean connect(String service);
 
 	/**
-	 * Sends a file to a cloud service.
+	 * Changes a file on a cloud service.
 	 * 
-	 * @return true, if the file could be send; false, if not.
+	 * @return true, if the file could be changed; false, if not.
 	 */
-	public boolean put();
+	public boolean put(String resource);
 
 	/**
 	 * Gets a file from a cloud service.
 	 * 
 	 * @return An InputStream to the regarding file.
 	 */
-	public InputStream get();
+	public InputStream get(String resource);
+
+	/**
+	 * Deletes a file on a cloud service.
+	 * 
+	 * @return true, if the file could be deleted; false, if not.
+	 */
+	public boolean delete(String resource);
+
+	/**
+	 * Sends a file to a cloud service.
+	 * 
+	 * @return The link to the new file on the cloud service.
+	 */
+	public String post(String resource, String parent);
+
+	/**
+	 * Returns the options available for a resource.
+	 * 
+	 * @return The options.
+	 */
+	public String[] options(String resource);
+
+	/**
+	 * Returns meta data for a resource.
+	 * 
+	 * @return The meta data.
+	 */
+	public String head(String resource);
 
 }
