5a7c90e1eb0b139c47b36f27dd2d1f362e2e9a98
Markus Holtermann <markus@markusholtermann.eu>
2011-12-18 01:22:20 +0100

    fix comments and whitespaces in comments; realign block / function comments

--- src/native/sha256.h
+++ src/native/sha256.h
@@ -10,11 +10,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -41,7 +41,9 @@ extern "C"
 {
 #endif
 
-    /* Structure to save state of computation between the single steps.  */
+    /**
+     * Structure to save state of computation between the single steps.
+     */
     struct sha256_ctx
     {
         uint32_t state[8];
@@ -51,45 +53,59 @@ extern "C"
         uint32_t buffer[32];
     };
 
-    /* Initialize structure containing state of computation. */
+    /*
+     * Initialize structure containing state of computation.
+     */
     extern void sha256_init_ctx ( struct sha256_ctx *ctx );
 
-    /* Starting with the result of former calls of this function (or the
-       initialization function update the context for the next LEN bytes
-       starting at BUFFER.
-       It is necessary that LEN is a multiple of 64!!! */
+    /**
+     * Starting with the result of former calls of this function (or the
+     * initialization function update the context for the next LEN bytes
+     * starting at BUFFER.
+     * It is necessary that LEN is a multiple of 64!!!
+     */
     extern void sha256_process_block ( const void *buffer, size_t len,
                                            struct sha256_ctx *ctx );
 
-    /* Starting with the result of former calls of this function (or the
-       initialization function update the context for the next LEN bytes
-       starting at BUFFER.
-       It is NOT required that LEN is a multiple of 64. */
+    /**
+     * Starting with the result of former calls of this function (or the
+     * initialization function update the context for the next LEN bytes
+     * starting at BUFFER.
+     * It is NOT required that LEN is a multiple of 64.
+     */
     extern void sha256_process_bytes ( const void *buffer, size_t len,
                                            struct sha256_ctx *ctx );
 
-    /* Process the remaining bytes in the buffer and put result from CTX
-       in first 32 bytes following RESBUF. The result is always in little
-       endian byte order, so that a byte-wise output yields to the wanted
-       ASCII representation of the message digest. */
+    /**
+     * Process the remaining bytes in the buffer and put result from CTX
+     * in first 32 bytes following RESBUF. The result is always in little
+     * endian byte order, so that a byte-wise output yields to the wanted
+     * ASCII representation of the message digest.
+     */
     extern void *sha256_finish_ctx ( struct sha256_ctx *ctx, void *resbuf );
 
 
-    /* Put result from CTX in first 32  bytes following RESBUF. The result is
-       always in little endian byte order, so that a byte-wise output yields
-       to the wanted ASCII representation of the message digest. */
+    /**
+     * Put result from CTX in first 32 bytes following RESBUF. The result is
+     * always in little endian byte order, so that a byte-wise output yields
+     * to the wanted ASCII representation of the message digest.
+     */
     extern void *sha256_read_ctx ( const struct sha256_ctx *ctx, void *resbuf );
 
 
-    /* Compute SHA256 message digest for bytes read from STREAM. The
-       resulting message digest number will be written into the 32 bytes
-       beginning at RESBLOCK. */
+    /**
+     * Compute SHA256 message digest for bytes read from STREAM. The
+     * resulting message digest number will be written into the 32 bytes
+     * beginning at RESBLOCK.
+     */
     extern int sha256_stream ( FILE *stream, void *resblock );
 
-    /* Compute SHA256 message digest for LEN bytes beginning at BUFFER. The
-       result is always in little endian byte order, so that a byte-wise
-       output yields to the wanted ASCII representation of the message
-       digest. */
+    /**
+     * Compute SHA256 message digest for LEN bytes beginning at BUFFER. The
+     * result is always in little endian byte order, so that a byte-wise
+     * output yields to the wanted ASCII representation of the message
+     * digest.
+     */
     extern void *sha256_buffer ( const char *buffer, size_t len, void *resblock );
 
 #ifdef __cplusplus
