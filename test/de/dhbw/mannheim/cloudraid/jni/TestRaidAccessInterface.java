f1b5f2b2d9e0402bd3cce9052cd5eb7ae825f1be
Markus Holtermann <markus@markusholtermann.de>
2012-01-07 13:37:34 +0100

    add junit test for input and output correctness of raid file

--- test/de/dhbw/mannheim/cloudraid/jni/TestRaidAccessInterface.java
+++ test/de/dhbw/mannheim/cloudraid/jni/TestRaidAccessInterface.java
@@ -21,9 +21,11 @@
 
 package de.dhbw.mannheim.cloudraid.jni;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -41,18 +43,29 @@ public class TestRaidAccessInterface {
 
 	private static File in, out;
 	private static String hash;
+	private static char[] content_array;
+	private static String content;
 
-	private static final String content = "CloudRAID TestRaidAccessInterface";
+	private static final int CONTENT_LENGTH = 1024 * 20;
 	private static final String tmpPath = System.getProperty("java.io.tmpdir")
 			+ File.separator;
-	private static final String key = "CloudRAID";
-	private static final int keyLength = 9;
+	private static final String KEY = "CloudRAID";
+	private static final int KEY_LENGTH = KEY.length();
 
 	@BeforeClass
 	public static void oneTimeSetUp() throws IOException {
+		int i;
 		in = new File(tmpPath, "TestRaidAccessInterface.in");
 		out = new File(tmpPath, "TestRaidAccessInterface.out");
 		FileWriter fw = new FileWriter(in);
+		content_array = new char[CONTENT_LENGTH];
+
+		for (i = 0; i < CONTENT_LENGTH; i++) {
+			content_array[i] = (char) (i % 256);
+		}
+
+		content = new String(content_array);
+
 		fw.write(content);
 		fw.close();
 	}
@@ -70,7 +83,7 @@ public class TestRaidAccessInterface {
 	public void testSplit() throws NoSuchAlgorithmException,
 			UnsupportedEncodingException {
 		hash = RaidAccessInterface.splitInterface(in.getAbsolutePath(),
-				tmpPath, key, keyLength);
+				tmpPath, KEY, KEY_LENGTH);
 
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		digest.reset();
@@ -86,7 +99,25 @@ public class TestRaidAccessInterface {
 	@Test
 	public void testMerge() {
 		int i = RaidAccessInterface.mergeInterface(tmpPath, hash,
-				out.getAbsolutePath(), key, keyLength);
+				out.getAbsolutePath(), KEY, KEY_LENGTH);
 		assertEquals(0x02, i);
+	}
+
+	@Test
+	public void testContent() throws IOException {
+		FileReader fr;
+		char[] buff;
+
+		fr = new FileReader(in);
+		buff = new char[CONTENT_LENGTH];
+		fr.read(buff, 0, CONTENT_LENGTH);
+		assertEquals(content, new String(buff));
+		fr.close();
+
+		fr = new FileReader(out);
+		buff = new char[CONTENT_LENGTH];
+		fr.read(buff, 0, CONTENT_LENGTH);
+		assertEquals(content, new String(buff));
+		fr.close();
 	}
 }
