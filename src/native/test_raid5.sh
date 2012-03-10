7aaae2eb454810a705428cd3f2983e86193925fb
Markus Holtermann <studium@markusholtermann.eu>
2012-03-10 20:14:10 +0100

    Fix the 'nothing to build' problem for 'make test' if the output folder already exists

--- src/native/test_raid5.sh
+++ src/native/test_raid5.sh
@@ -4,9 +4,9 @@ for i in {0..28} ; do
     BS=$(($i*256))
     gcc test_raid5.c -DCHECKING=0 -DBENCHSIZE=${BS} -DENCRYPT_DATA=1 \
         -Wall -pedantic -g "./build/usr/lib/libcloudraid.so" \
-        -o "./test/test_raid5_${BS}"
+        -o "./testing/test_raid5_${BS}"
 
-    ./test/test_raid5_${BS} > /dev/null
+    ./testing/test_raid5_${BS} > /dev/null
 
     in=$(sha256sum test_raid5.dat)
     out=$(sha256sum test_raid5.out.dat)
