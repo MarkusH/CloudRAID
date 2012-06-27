8ed5772938463c9dc515bfa7043d2d118be05944
Markus Holtermann <studium@markusholtermann.eu>
2012-06-27 10:24:43 +0200

    Dynamically gather and startup the storage connectors based on the configuration settings

--- core/src/de/dhbw_mannheim/cloudraid/core/impl/CloudRAIDService.java
+++ core/src/de/dhbw_mannheim/cloudraid/core/impl/CloudRAIDService.java
@@ -25,9 +25,14 @@ package de.dhbw_mannheim.cloudraid.core.impl;
 import java.io.File;
 import java.util.NoSuchElementException;
 
+import javax.management.ServiceNotFoundException;
 import javax.naming.directory.InvalidAttributeValueException;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 import de.dhbw_mannheim.cloudraid.config.ICloudRAIDConfig;
 import de.dhbw_mannheim.cloudraid.config.exceptions.ConfigException;
@@ -35,6 +40,7 @@ import de.dhbw_mannheim.cloudraid.config.exceptions.MissingConfigValueException;
 import de.dhbw_mannheim.cloudraid.core.ICloudRAIDService;
 import de.dhbw_mannheim.cloudraid.core.impl.fs.FileManager;
 import de.dhbw_mannheim.cloudraid.core.impl.fs.RecursiveFileSystemWatcher;
+import de.dhbw_mannheim.cloudraid.core.net.connector.IStorageConnector;
 
 /**
  * @author Markus Holtermann
@@ -47,6 +53,17 @@ public class CloudRAIDService implements ICloudRAIDService {
 	private FileManager[] fileManagers = null;
 
 	private RecursiveFileSystemWatcher recursiveFileSystemWatcher = null;
+
+	private IStorageConnector[] storageConnectors = { null, null, null };
+	private String[] storageConnectorClassnames = { null, null, null };
+
+	private String splitInputDir;
+
+	private String mergeInputDir;
+
+	private String mergeOutputDir;
+
+	private String splitOutputDir;
 
 	/**
 	 * @param config
@@ -95,31 +112,90 @@ public class CloudRAIDService implements ICloudRAIDService {
 	 * @throws InvalidAttributeValueException
 	 *             Thrown, if the watching directory for the
 	 *             {@link RecursiveFileSystemWatcher} is invalid.
+	 * @throws InvalidSyntaxException
+	 * @throws ClassNotFoundException
+	 * @throws BundleException
+	 * @throws ServiceNotFoundException
 	 */
-	protected void startup(BundleContext context)
-			throws NoSuchElementException, ConfigException,
-			InvalidAttributeValueException {
+	protected void startup(BundleContext context) throws ConfigException,
+			InvalidAttributeValueException, ClassNotFoundException {
 		System.out.println("CloudRAIDService: startup: begin");
 
-		String mergeInputDir = config.getString("merge.input.dir", null);
-		String mergeOutputDir = config.getString("merge.output.dir", null);
-		String splitInputDir = config.getString("split.input.dir", null);
-		String splitOutputDir = config.getString("split.output.dir", null);
+		initPaths();
 
-		if (mergeInputDir == null || mergeOutputDir == null
-				|| splitInputDir == null || splitOutputDir == null) {
-			throw new MissingConfigValueException(
-					"Missing split or merge directory definitions.");
+		String classname = null;
+		for (int i = 0; i < 3; i++) {
+			classname = this.config.getString(String.format("connector.%d", i));
+			if (classname == null || classname.isEmpty()) {
+			} else {
+				this.storageConnectorClassnames[i] = classname;
+			}
 		}
 
-		new File(mergeInputDir).mkdirs();
-		new File(mergeOutputDir).mkdirs();
-		new File(splitInputDir).mkdirs();
-		new File(splitOutputDir).mkdirs();
+		int connectedServices = 0;
 
+		System.out
+				.println("Trying to resolve and start the required bundles and services ...");
+		Bundle[] bundles = context.getBundles();
+		for (Bundle b : bundles) {
+			for (int i = 0; i < 3 && connectedServices < 3; i++) {
+
+				try {
+					b.loadClass(this.storageConnectorClassnames[i]);
+					Thread.sleep(2500);
+					if (b.getState() != Bundle.ACTIVE) {
+						System.out.println("\tStarting bundle "
+								+ b.getSymbolicName()
+								+ " and wait 5 seconds ...");
+						b.start();
+						Thread.sleep(2500);
+						System.out.println("\t\tDone");
+					} else {
+						System.out.println("\tBundle " + b.getSymbolicName()
+								+ " already active.");
+					}
+
+					ServiceReference<?>[] scs = b.getRegisteredServices();
+					if (scs == null || scs.length < 1) {
+						throw new ServiceNotFoundException(
+								"\tThe service for class "
+										+ this.storageConnectorClassnames[i]
+										+ " could not be found!");
+					}
+					IStorageConnector sc = (IStorageConnector) context
+							.getService(scs[0]);
+					if (this.storageConnectorClassnames[i].equals(sc.getClass()
+							.getName()) && this.storageConnectors[i] == null) {
+						this.storageConnectors[i] = sc;
+						connectedServices++;
+					}
+
+				} catch (BundleException e) {
+					System.err.println("Cannot start bundle " + b);
+					e.printStackTrace();
+				} catch (Exception e) {
+					// System.err.println("Class "
+					// + this.storageConnectorClassnames[i]
+					// + " not found in bundle " + b.getSymbolicName());
+				}
+
+			}
+			if (connectedServices == 3) {
+				break;
+			}
+		}
+		if (connectedServices == 3) {
+			System.out
+					.println("Resolved and started all required cloud connector bundles.");
+		} else {
+			System.err
+					.println("Couldn't resolve or start all required bundles!");
+			throw new ClassNotFoundException(
+					"Couldn't resolve or start all required bundles!");
+		}
 		recursiveFileSystemWatcher = new RecursiveFileSystemWatcher(
-				config.getString("split.input.dir", null), config.getInt(
-						"filemanagement.intervall", 60000));
+				this.splitInputDir, config.getInt("filemanagement.intervall",
+						60000));
 		recursiveFileSystemWatcher.start();
 
 		int proc = config.getInt("filemanagement.count", 1);
@@ -145,4 +221,21 @@ public class CloudRAIDService implements ICloudRAIDService {
 		System.out.println("CloudRAIDService: unsetConfig: end");
 	}
 
+	private void initPaths() throws MissingConfigValueException {
+		this.mergeInputDir = this.config.getString("merge.input.dir");
+		this.mergeOutputDir = this.config.getString("merge.output.dir");
+		this.splitInputDir = this.config.getString("split.input.dir");
+		this.splitOutputDir = this.config.getString("split.output.dir");
+
+		if (this.mergeInputDir == null || this.mergeOutputDir == null
+				|| this.splitInputDir == null || this.splitOutputDir == null) {
+			throw new MissingConfigValueException(
+					"Missing split or merge directory definitions.");
+		}
+
+		new File(this.mergeInputDir).mkdirs();
+		new File(this.mergeOutputDir).mkdirs();
+		new File(this.splitInputDir).mkdirs();
+		new File(this.splitOutputDir).mkdirs();
+	}
 }
